4970b39ab75631adc10921d2da1f9513647d4855
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-06 09:23:55 -0400

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // get user left - right input
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        // get the current position
+        Vector2 currentPosition = transform.position;
+
+        // calculate new position
+        Vector2 newPosition = currentPosition + new Vector2(horizontalInput * speed * Time.deltaTime, 0);
+
+        // transform to new position
+        transform.position = newPosition;
+    }
+}
