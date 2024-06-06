d35536c1ea4491c9ddd5620b2c44d1d81c59c200
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-06 13:52:53 -0400

    Add reset position and script when Player comes in contact with a Goal

--- Assets/Scripts/PossibleGoal.cs
+++ Assets/Scripts/PossibleGoal.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 
 public class PossibleGoal : MonoBehaviour
 {
+    public gameObject targetObject;
+    
     public void Modify()
     {
         // change object colour to white
         GetComponent<SpriteRenderer>().color = Color.white;
     }
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && GetComponent<SpriteRenderer>().color == Color.white)
         {
             Debug.Log("Win!");
+            PlayerMovement targetScript = targetObject.GetComponent<PlayerMovement>();
+            targetObject.transform.position = new Vector2(0, 0);
+            targetScript.falling = false;
         }
         else if (collision.gameObject.CompareTag("Player") && GetComponent<SpriteRenderer>().color == Color.red)
         {
