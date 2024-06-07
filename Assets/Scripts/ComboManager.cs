e64e7146a3927cd42f65acc783ca47765b0cd9d2
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-07 13:05:17 -0400

    Create ComboManager.cs

---
+++ Assets/Scripts/ComboManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboManager : MonoBehaviour
+{
+    public int combo = 0;
+    public TextMeshPro comboText;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+    }
+    private void UpdateCombo()
+    {
+
+    }
+}
