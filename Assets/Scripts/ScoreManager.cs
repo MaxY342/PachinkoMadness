4646bbd7477770c57b3f3c2b3342dc82e70c62e3
MaxY342 <max.yang342@gmail.com>
2024-06-07 05:23:32 -0400

    update ScoreManager.cs to allow for TextMeshPro

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     private int score;
     // Start is called before the first frame update
     void Start()
