1ea7285044fd246ed1751fbddf07a4e509f451f5
MaxY342 <max.yang342@gmail.com>
2024-06-07 05:02:15 -0400

    updated ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager instance;
+    public Text scoreText;
+    private int score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        UpdateScoreText();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
     }
 }
