3d25b3a2ddffdcedee0c37aae7441f4961d6d6ca
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-07 13:09:29 -0400

    Update ComboManager.cs

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComboManager : MonoBehaviour
 {
-    public int combo = 0;
-    public TextMeshPro comboText;
+    public TextMeshProUGUI comboText;
+    private int combo;
+    // Start is called before the first frame update
     void Start()
+    {
+        combo = 0;
+        UpdateComboText();
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         
     }
-    void Update()
+    public void AddScore(int comboValue)
     {
-
+        combo += comboValue;
+        Debug.Log("Combo added: " + comboValue);
+        UpdateComboText();
     }
-    private void UpdateCombo()
+    private void UpdateComboText()
     {
-
+        comboText.text = "Combo: " + combo;
     }
 }
