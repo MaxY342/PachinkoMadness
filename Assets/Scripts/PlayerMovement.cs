cfd32610eb593dacc5057fe518fbe01ab15ef583
MaxY342 <max.yang342@gmail.com>
2024-06-07 04:13:36 -0400

    fixed PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,35 +8,45 @@ public class PlayerMovement : MonoBehaviour
     private bool falling = false;
     private Rigidbody2D rb;
     private Vector2 initialPosition;
+
     void Start()
     {
         initialPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
     }
+
     void Update()
     {
         if (!falling)
         {
             // get user left - right input
             float horizontalInput = Input.GetAxis("Horizontal");
-    
+
             // calculate new position
             Vector2 newPosition = new Vector2(horizontalInput * speed * Time.deltaTime, 0);
-    
-            // transform to new position
+
+            // move to new position
             transform.Translate(newPosition);
         }
-        if (Input.GetKeyDown("space"))
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             falling = true;
             rb.gravityScale = 1;
         }
     }
+
     public void ResetObject()
     {
+        // Reset position
         transform.position = initialPosition;
+
+        // Reset falling status
         falling = false;
+
+        // Stop any existing movement
+        rb.velocity = Vector2.zero;
         rb.gravityScale = 0;
     }
 }
