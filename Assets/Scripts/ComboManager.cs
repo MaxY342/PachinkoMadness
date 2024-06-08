feb7902f35f1425a411d15e13e523dede3cd4cf1
MaxY342 <max.yang342@gmail.com>
2024-06-08 16:35:01 -0400

    added fire for combo effect

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -7,7 +7,11 @@ using UnityEngine.UI;
 public class ComboManager : MonoBehaviour
 {
     public TextMeshProUGUI comboText;
+    public GameObject firePreFab;
+    public Transform canvasTransform;
+
     private int combo;
+    private GameObject currentFlame;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +23,41 @@ public class ComboManager : MonoBehaviour
     {
         
     }
+
     public void AddCombo(int comboValue)
     {
         combo += comboValue;
         Debug.Log("Combo added: " + comboValue);
         UpdateComboText();
+        ShowFlame();
     }
+
     private void UpdateComboText()
     {
-        comboText.text = "Combo: " + combo;
+        comboText.text = "Combo: " + combo + "x";
     }
+
     public void ResetCombo()
     {
         combo = 0;
         UpdateComboText();
+        HideFlame();
+    }
+
+    private void ShowFlame()
+    {
+        if (currentFlame == null)
+        {
+            currentFlame = Instantiate(firePreFab, canvasTransform);
+        }
+        currentFlame.SetActive(true);
+    }
+
+    private void HideFlame()
+    {
+        if (currentFlame != null)
+        {
+            currentFlame.SetActive(false);
+        }
     }
 }
