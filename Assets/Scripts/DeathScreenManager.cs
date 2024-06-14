72084e17f93e5252e9922964afdaf5248edb3385
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-14 09:42:52 -0400

    Create DeathScreenManager.cs

---
+++ Assets/Scripts/DeathScreenManager.cs
@@ -0,0 +1,26 @@
+using System.Collections
+using UnityEngine
+using UnityEngine.UI
+
+public class DeathScreenManager : Monobehaviour
+{
+    public GameObject deathScreen;
+    private float displayTime = 3.0f;
+
+    void Start()
+    {
+        deathScreen.SetActive(false);
+    }
+    public void ShowDeathScreen()
+    {
+        StartCoroutine(DisplayDeathScreen());
+    }
+
+    private IEnumerator DisplayDeathScreen()
+    {
+        deathScreen.SetActive(true);
+
+        yield return new WaitForSeconds(waitTime);
+        
+    }
+}
