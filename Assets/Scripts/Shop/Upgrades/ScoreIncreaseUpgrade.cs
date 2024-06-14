26831ffdeef9664589bd92a063a889eda6ba41c6
MaxY342 <max.yang342@gmail.com>
2024-06-14 19:56:24 -0400

    Update ScoreIncreaseUpgrade.cs

--- Assets/Scripts/Shop/Upgrades/ScoreIncreaseUpgrade.cs
+++ Assets/Scripts/Shop/Upgrades/ScoreIncreaseUpgrade.cs
@@ -3,10 +3,11 @@ using UnityEngine.UI;
 
 public class ScoreIncreaseUpgrade : MonoBehaviour
 {
+    public gameObject upgradeCard;
     private int increaseRate = 1;
 
-    public void IncreaseScoreUpgrade(PossibleGoal scoreValue)
+    public void IncreaseScore(PossibleGoal possibleGoal)
     {
-        
+        possibleGoal.ScoreValue += increaseRate;
     }
 }
