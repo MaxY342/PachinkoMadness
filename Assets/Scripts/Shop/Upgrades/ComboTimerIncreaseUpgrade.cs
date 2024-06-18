aadcb135a2d008cb731b4c11fe7c7f814ffc6dea
MaxY342 <156880402+MaxY342@users.noreply.github.com>
2024-06-18 14:21:34 -0400

    Update ComboTimerIncreaseUpgrade.cs

--- Assets/Scripts/Shop/Upgrades/ComboTimerIncreaseUpgrade.cs
+++ Assets/Scripts/Shop/Upgrades/ComboTimerIncreaseUpgrade.cs
@@ -4,9 +4,9 @@ using UnityEngine.UI;
 public class ComboTimerIncreaseUpgrade : MonoBehaviour
 {
     private float increaseRate = 1.5f;
-    private float currentComboTimer = ScoreManager.instance.GetComboTimer();
+    private float currentComboTimer = ComboManager.instance.GetComboTimer();
     public void IncreaseComboTimer()
     {
-        ScoreManager.instance.SetComboTimer(currentComboTimer + increaseRate);
+        ComboManager.instance.SetComboTimer(currentComboTimer + increaseRate);
     }
 }
